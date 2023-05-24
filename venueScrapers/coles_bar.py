63296be511c28bb542b267bcfc7113f1b7fd384d
matt manes <mattmanes@pm.me>
2023-05-24 13:33:37 -0500

    fix: update scraper to new calendar

--- venueScrapers/coles_bar.py
+++ venueScrapers/coles_bar.py
@@ -1,64 +1,56 @@
-import datetime
+import json
+import re
+from datetime import datetime
 from pathlib import Path
 
-from gig_scraper_engine import GigScraper, get_soup
+from gig_scraper_engine import GigScraper, get_page, get_soup
 
 
+# https://www.colesbarchicago.com/
 class Scraper(GigScraper):
     def __init__(self):
         super().__init__(Path(__file__))
 
+    def get_event_links(self) -> list[str]:
+        soup = get_soup("https://www.colesbarchicago.com/")
+        events = []
+        for div in soup.find_all("div", class_="image-wrapper"):
+            try:
+                url = div.find("a").get("href")
+                events.append(url)
+            except Exception as e:
+                pass
+        return events
+
+    def get_event_data(self, url: str) -> dict | None:
+        soup = get_soup(url)
+        data = None
+        for script in soup.find_all("script"):
+            if "General Data Points" in script.text:
+                data = script.text
+                break
+        if not data:
+            return data
+        data = re.findall(r"// Track[^;]+", data)[0]
+        data = data[data.find("{") : data.rfind("}") + 1].replace("'", '"')
+        return json.loads(data)["ecommerce"]["detail"]["products"][0]
+
     def scrape(self):
         self.logger.info("Scrape started")
         try:
-            base_url = self.venue_info["website"]
-            url = self.venue_info["calendar_url"]
-            soup = get_soup(url)
-            events = soup.find("div", attrs={"id": "productList"}).find_all("a")
+            events = self.get_event_links()
             for event in events:
                 self.reset_event_details()
-                self.event_link = f'{base_url.strip("/")}{event.get("href")}'
-                soup = get_soup(self.event_link)
-                self.title = soup.find("title").text.replace("— Cole's Bar", "")
-                date_string = self.title[: self.title.find(":")].lower()
-                if "cole's holiday market" in date_string or "new years" in date_string:
-                    continue
-                try:
-                    self.event_date = datetime.datetime.strptime(
-                        f"{self.today.year} {date_string} 10:00 pm",
-                        "%Y %A %B %d %I:%M %p",
-                    )
-                except:
-                    try:
-                        self.event_date = datetime.datetime.strptime(
-                            f"{self.today.year} {date_string} 10:00 pm",
-                            "%Y %A %B %dth %I:%M %p",
-                        )
-                    except Exception as e:
-                        try:
-                            self.event_date = datetime.datetime.strptime(
-                                f"{self.today.year} {date_string} 10:00 pm",
-                                "%Y %A %B %dst %I:%M %p",
-                            )
-                        except Exception as e:
-                            self.event_date = datetime.datetime.strptime(
-                                f"{self.today.year} {date_string} 10:00 pm",
-                                "%Y %A %B %dnd %I:%M %p",
-                            )
-                self.check_event_date_year()
-                self.acts = self.title[self.title.find(":") + 2 :]
-                self.price = soup.find(
-                    "meta", attrs={"property": "product:price:amount"}
-                ).get("content")
-                product_excerpt = soup.find("div", class_="product-excerpt")
-                if product_excerpt:
-                    self.info = "\n".join(ele.text for ele in product_excerpt.children)
-                    self.act_links = "\n".join(
-                        ele.get("href")
-                        for ele in product_excerpt.find_all("a")
-                        if "spotify" not in ele.get("href")
-                    )
-                self.add_event()
+                data = self.get_event_data(event)
+                self.event_date = datetime.strptime(
+                    data["variant"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                )
+                if self.event_date > datetime.now():
+                    self.title = data["name"]
+                    self.acts = self.title
+                    self.price = f"${data['priceDetails'][0]['price']}"
+                    self.event_link = event
+                    self.add_event()
             self.log_success()
         except:
             self.logger.exception("Scrape failed")
