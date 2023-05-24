1e3ed9badff60d66754bde540a29452416bca4af
matt manes <mattmanes@pm.me>
2023-05-24 12:28:30 -0500

    fix: update scraper to new calendar

--- venueScrapers/the_burlington.py
+++ venueScrapers/the_burlington.py
@@ -1,45 +1,49 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
+import json
+from typing import Iterable
 
-from gig_scraper_engine import GigScraper, get_soup, get_text
-
+from gig_scraper_engine import GigScraper, get_soup, get_text, get_page
 
 # https://www.theburlingtonbar.com/
 class Scraper(GigScraper):
     def __init__(self):
         super().__init__(Path(__file__))
 
+    def get_calendar(self, collection_id: str) -> Iterable[dict]:
+        date = datetime.now()
+        counter = 0
+        while True:
+            month_year = f"{date:%m-%Y}"
+            url = f"https://www.theburlingtonbar.com/api/open/GetItemsByMonth?month={month_year}&collectionId={collection_id}"
+            response = get_page(url)
+            if len(response.content) == 2 or counter >= 12:
+                break
+            yield response.json()
+            date += timedelta(weeks=4)
+            counter += 1
+
     def scrape(self):
         self.logger.info("Scrape started")
         try:
-            for i in range(3):
-                if i == 0:
-                    soup = get_soup(self.venue_info["calendar_url"])
-                else:
-                    next_month = (
-                        soup.find("div", class_="nextmonth").a.get("href").strip("/")
-                    )
-                    soup = get_soup(f'{self.venue_info["calendar_url"]}{next_month}')
-                events = soup.find_all("td", class_="cal_dayshasevents")
-                for event in events:
+            soup = get_soup(self.venue_info["calendar_url"])
+            collection_id = json.loads(
+                soup.find(
+                    "div", class_="sqs-block calendar-block sqs-block-calendar"
+                ).get("data-block-json")
+            )["collectionId"]
+            for month in self.get_calendar(collection_id):
+                for event in month:
                     self.reset_event_details()
-                    date_link = event.a.get("href")[1:]
-                    date_link = date_link[
-                        date_link.find("/") + 1 : date_link.rfind("/")
-                    ]
-                    self.event_date = datetime.strptime(
-                        f"{date_link} 8pm", "%Y/%m/%d %I%p"
-                    )
-                    self.title = get_text(event.find("a", class_="cal_titlelink"))
+                    self.event_date = datetime.fromtimestamp(event["startDate"] / 1000)
+                    self.title = event["title"]
                     self.acts = self.title
                     self.price = "$10"
-                    event_link = (
-                        event.find("a", class_="cal_titlelink").get("href").strip("/")
-                    )
-                    self.event_link = f'{self.venue_info["website"]}{event_link}'
+                    self.event_link = f"{self.venue_info['calendar_url'].strip('/')}{event['fullUrl']}"
                     self.add_event()
             self.log_success()
         except Exception as e:
+            print(e)
             self.logger.exception(e)
 
 
