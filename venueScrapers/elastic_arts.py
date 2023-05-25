3875e11fcf9f0713da54eea115b3e952f76628c0
matt manes <mattmanes@pm.me>
2023-05-24 20:17:48 -0500

    perf: access calendar api directly

--- venueScrapers/elastic_arts.py
+++ venueScrapers/elastic_arts.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from pathlib import Path
+import re
 
-from gig_scraper_engine import GigScraper, get_soup, get_text
+from gig_scraper_engine import GigScraper, get_soup, get_text, get_page
 
-
+# https://elasticarts.org/events
 class Scraper(GigScraper):
     def __init__(self):
         super().__init__(Path(__file__))
@@ -11,37 +12,23 @@ class Scraper(GigScraper):
     def scrape(self):
         self.logger.info("Scrape started")
         try:
-            soup = get_soup(self.venue_info["calendar_url"])
-            event_links = [
-                f'{self.venue_info["website"].strip("/")}{event.get("href")}'
-                for event in soup.find_all("a", class_="eventlist-title-link")
-            ]
-            for event_link in event_links:
-                self.reset_event_details()
-                soup = get_soup(event_link)
-                date = soup.find("time", class_="event-date").get("datetime")
-                time = get_text(soup.find("time", class_="event-time-localized-start"))
-                if time == "":
-                    time = get_text(soup.find("time", class_="event-time-localized"))
-                self.event_date = datetime.strptime(
-                    f"{date} {time}", "%Y-%m-%d %I:%M %p"
-                )
-                self.title = get_text(
-                    soup.find("h1", attrs={"data-content-field": "title"})
-                )
-                self.acts = self.title
-                info = (
-                    soup.find("div", class_="eventitem-column-content")
-                    .div.div.find_all("div")[-1]
-                    .text
-                )
-                self.price = info[info.find("$") : info.find(" ", info.find("$"))]
-                self.event_link = event_link
-                self.info = info
-                self.add_event()
+            page = get_page(self.venue_info["calendar_url"])
+            collection_id = re.findall(
+                r'collectionId":"[a-zA-Z0-9]+"', page.text.replace("&quot;", '"')
+            )[0].split('"')[2]
+            for month in self.get_squarespace_calendar(collection_id):
+                for event in month:
+                    self.reset_event_details()
+                    self.event_date = datetime.fromtimestamp(event["startDate"] / 1000)
+                    self.title = event["title"]
+                    self.price = "$15+"
+                    self.event_link = (
+                        f"{self.venue_info['website'].strip('/')}{event['fullUrl']}"
+                    )
+                    self.add_event()
             self.log_success()
         except:
-            self.logger.exception(f"Scrape failed on {event_link}")
+            self.logger.exception(f"Scrape failed.")
 
 
 if __name__ == "__main__":
