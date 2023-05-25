4dcb8ea0885ef53019216e6e5316373a2b991c26
matt manes <mattmanes@pm.me>
2023-05-24 19:01:55 -0500

    fix: fix scraper not scraping

--- venueScrapers/liars_club.py
+++ venueScrapers/liars_club.py
@@ -1,9 +1,9 @@
 import time
 from datetime import datetime
 from pathlib import Path
+import json
 
-from gig_scraper_engine import GigScraper, get_text
-from seleniumuser import User
+from gig_scraper_engine import GigScraper, get_text, get_page
 
 
 class Scraper(GigScraper):
@@ -12,40 +12,30 @@ class Scraper(GigScraper):
 
     def scrape(self):
         self.logger.info("Scrape started")
-        user = User(headless=True)
         try:
-            user.get(self.venue_info["calendar_url"])
-            time.sleep(5)
-            soup = user.get_soup()
-            event_links = [
-                f"{self.venueInfo['website']}{ele.get('href').strip('/')}"
-                for ele in soup.find_all("a", class_="product-group__link")
-            ]
-            for event_link in event_links:
+            data = get_page(
+                "https://cdn5.editmysite.com/app/store/api/v28/editor/users/131312941/sites/715381155365579768/products?per_page=60&categories[]=11eb8686dd0a0704b0c60cc47a2b63cc"
+            ).json()["data"]
+            for event in data:
                 self.reset_event_details()
-                user.get(event_link)
-                time.sleep(5)
-                soup = user.get_soup()
-                date = get_text(soup.find("span", class_="event-date-range__start"))
+                details = event["product_type_details"]
                 self.event_date = datetime.strptime(
-                    f"{self.today.year} {date}", "%Y %a, %b %d, %I:%M %p"
+                    f"{details['start_date']} {details['start_time']}",
+                    "%Y-%m-%d %H:%M %p",
                 )
-                self.check_event_date_year()
-                self.title = get_text(soup.find("div", class_="product__title"))
-                self.acts = self.title[self.title.find("-") + 2 :]
-                self.price = get_text(
-                    soup.find("div", class_="product__header").find("h3")
-                )
-                self.event_link = event_link
-                self.info = get_text(
-                    soup.find("div", class_="w-wrapper product-description")
-                )
+                self.title = event["name"]
+                high = event["price"]["high_formatted"]
+                low = event["price"]["low_formatted"]
+                if high == low:
+                    self.price = high
+                else:
+                    self.price = f"{low}-{high}"
+                self.event_link = f"{self.venue_info['website']}{event['site_link']}"
                 self.add_event()
 
             self.log_success()
         except:
-            self.logger.exception(f"Scrape failed on {event_link}")
-        user.close_browser()
+            self.logger.exception(f"Scrape failed")
 
 
 if __name__ == "__main__":
