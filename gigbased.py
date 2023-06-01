33f08434504d1615ab28b91941d08534abd0b431
matt manes <mattmanes@pm.me>
2023-05-31 19:50:25 -0500

    initial commit

---
+++ gigbased.py
@@ -0,0 +1,152 @@
+import dacite
+from databased import DataBased
+
+import models
+
+
+class GigBased(DataBased):
+    def __init__(self, dbpath="getToTheGig.db"):
+        super().__init__(dbpath)
+        self.create_tables
+
+    def create_tables(self):
+        self.create_venues_table()
+        self.create_events_table()
+
+    def create_venues_table(self):
+        self.create_table(
+            "venues",
+            [
+                "name text unique",
+                "street text",
+                "city text",
+                "state text",
+                "zip_code text",
+                "website text",
+                "calendar_url text",
+                "reference text unique",
+                "date_added timestamp",
+            ],
+        )
+
+    def create_events_table(self):
+        self.create_table(
+            "events",
+            [
+                "venue text",
+                "title text",
+                "date timestamp",
+                "acts text",
+                "price text",
+                "event_url text",
+                "ticket_url text",
+                "act_urls text",
+                "info text",
+                "age_restriction text",
+                "date_added timestamp",
+                "genres text",
+                "in_the_future int",
+            ],
+        )
+
+    def add_venue(self, venue: models.Venue) -> bool:
+        """Add a venue to the database.
+
+        Returns `True` if successful."""
+        return self.add_row(
+            "venues",
+            [
+                venue.name,
+                venue.address.street,
+                venue.address.city,
+                venue.address.state,
+                venue.address.zip_code,
+                venue.website,
+                venue.calendar_url,
+                venue.reference,
+                venue.date_added,
+            ],
+            [
+                "name",
+                "street",
+                "city",
+                "state",
+                "zip_code",
+                "website",
+                "calendar_url",
+                "reference",
+                "date_added",
+            ],
+        )
+
+    def add_event(self, event: models.Event) -> bool:
+        """Add an event to the database.
+
+        Returns `True` if successful."""
+        return self.add_row(
+            "events",
+            (
+                event.venue,
+                event.title,
+                event.date,
+                event.acts,
+                event.price,
+                event.event_url,
+                event.ticket_url,
+                event.act_urls,
+                event.info,
+                event.age_restriction,
+                event.date_added,
+                event.genres,
+                event.in_the_future,
+            ),
+            (
+                "venue",
+                "title",
+                "date",
+                "acts",
+                "price",
+                "event_url",
+                "ticket_url",
+                "act_urls",
+                "info",
+                "age_restriction",
+                "date_added",
+                "genres",
+                "in_the_future",
+            ),
+        )
+
+    def get_venues(self, *args, **kwargs) -> list[models.Venue]:
+        """Return a list of `Venue` models.
+
+        `*args` and `**kwargs` can be any parameters accepted by `self.get_rows()`, except `table`:
+
+        * `match_criteria: list[tuple] | dict | None = None`
+        * `exact_match: bool = True`
+        * `sort_by_column: str | None = None`
+        * `columns_to_return: list[str] | None = None`
+        * `return_as_dataframe: bool = False`
+        * `values_only: bool = False`
+        * `order_by: str | None = None`
+        * `limit: str | int | None = None`
+        """
+        rows = self.get_rows("venues", *args, **kwargs)
+        return [dacite.from_dict(models.Venue, row) for row in rows]
+
+    def get_events(self, *args, **kwargs) -> list[models.Event]:
+        """Return a list of `Event` models.
+
+        `*args` and `**kwargs` can be any parameters accepted by `self.get_rows()`, except `table`:
+
+        * `match_criteria: list[tuple] | dict | None = None`
+        * `exact_match: bool = True`
+        * `sort_by_column: str | None = None`
+        * `columns_to_return: list[str] | None = None`
+        * `return_as_dataframe: bool = False`
+        * `values_only: bool = False`
+        * `order_by: str | None = None`
+        * `limit: str | int | None = None`
+        """
+        rows = self.get_rows("events", *args, **kwargs)
+        return [dacite.from_dict(models.Event, row) for row in rows]
