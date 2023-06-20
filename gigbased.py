4a126b41685f1d5ffce4b9a23b7ab446a5f92b8b
matt manes <mattmanes@pm.me>
2023-06-20 17:40:12 -0500

    sort GigBased

--- gigbased.py
+++ gigbased.py
@@ -1,4 +1,3 @@
-from dataclasses import asdict
 from datetime import datetime
 
 import dacite
@@ -11,76 +10,6 @@ class GigBased(DataBased):
     def __init__(self, dbpath="getToTheGig.db"):
         super().__init__(dbpath)
         self.create_tables()
-
-    def create_tables(self):
-        self.create_venues_table()
-        self.create_events_table()
-
-    def create_venues_table(self):
-        self.create_table(
-            "venues",
-            [
-                "name text unique",
-                "street text",
-                "city text",
-                "state text",
-                "zip_code text",
-                "website text",
-                "calendar_url text",
-                "ref_name text",
-                "date_added timestamp",
-            ],
-        )
-
-    def create_events_table(self):
-        self.create_table(
-            "events",
-            [
-                "venue text",
-                "title text",
-                "date timestamp",
-                "acts text",
-                "price text",
-                "event_url text",
-                "ticket_url text",
-                "act_urls text",
-                "info text",
-                "age_restriction text",
-                "date_added timestamp",
-                "genres text",
-                "in_the_future int",
-            ],
-        )
-
-    def add_venue(self, venue: models.Venue) -> bool:
-        """Add a venue to the database.
-
-        Returns `True` if successful."""
-        return self.add_row(
-            "venues",
-            [
-                venue.name,
-                venue.address.street,
-                venue.address.city,
-                venue.address.state,
-                venue.address.zip_code,
-                venue.website,
-                venue.calendar_url,
-                venue.ref_name,
-                venue.date_added,
-            ],
-            [
-                "name",
-                "street",
-                "city",
-                "state",
-                "zip_code",
-                "website",
-                "calendar_url",
-                "ref_name",
-                "date_added",
-            ],
-        )
 
     def add_event(self, event: models.Event) -> bool:
         """Add an event to the database.
@@ -120,22 +49,87 @@ class GigBased(DataBased):
             ),
         )
 
-    def get_venues(self, *args, **kwargs) -> list[models.Venue]:
-        """Return a list of `Venue` models.
+    def add_venue(self, venue: models.Venue) -> bool:
+        """Add a venue to the database.
 
-        `*args` and `**kwargs` can be any parameters accepted by `self.get_rows()`, except `table`:
+        Returns `True` if successful."""
+        return self.add_row(
+            "venues",
+            [
+                venue.name,
+                venue.address.street,
+                venue.address.city,
+                venue.address.state,
+                venue.address.zip_code,
+                venue.website,
+                venue.calendar_url,
+                venue.ref_name,
+                venue.date_added,
+            ],
+            [
+                "name",
+                "street",
+                "city",
+                "state",
+                "zip_code",
+                "website",
+                "calendar_url",
+                "ref_name",
+                "date_added",
+            ],
+        )
 
-        * `match_criteria: list[tuple] | dict | None = None`
-        * `exact_match: bool = True`
-        * `sort_by_column: str | None = None`
-        * `columns_to_return: list[str] | None = None`
-        * `return_as_dataframe: bool = False`
-        * `values_only: bool = False`
-        * `order_by: str | None = None`
-        * `limit: str | int | None = None`
-        """
-        rows = self.get_rows("venues", *args, **kwargs)
-        return [dacite.from_dict(models.Venue, row) for row in rows]
+    def create_events_table(self):
+        self.create_table(
+            "events",
+            [
+                "venue text",
+                "title text",
+                "date timestamp",
+                "acts text",
+                "price text",
+                "event_url text",
+                "ticket_url text",
+                "act_urls text",
+                "info text",
+                "age_restriction text",
+                "date_added timestamp",
+                "genres text",
+                "in_the_future int",
+            ],
+        )
+
+    def create_tables(self):
+        self.create_venues_table()
+        self.create_events_table()
+
+    def create_venues_table(self):
+        self.create_table(
+            "venues",
+            [
+                "name text unique",
+                "street text",
+                "city text",
+                "state text",
+                "zip_code text",
+                "website text",
+                "calendar_url text",
+                "ref_name text",
+                "date_added timestamp",
+            ],
+        )
+
+    def drop_future_events(self, venue: models.Venue | None = None):
+        """Delete events that haven't happened yet for `venue`.
+        If `venue` is not given, the operation will occur on all venues.
+
+        #### :Intention: Avoid adding duplicate events or needing to determine
+        if the event to be added already exists but website information for it has changed.
+        Ideally a backup of the database should be made first and this should be called after calling `self.update_in_the_future`."""
+        if venue:
+            self.delete("events", {"venue": venue.name, "in_the_future": 1})
+        else:
+            self.delete("events", {"in_the_future": 1})
 
     def get_events(self, *args, **kwargs) -> list[models.Event]:
         """Return a list of `Event` models.
@@ -161,12 +155,22 @@ class GigBased(DataBased):
             models.Venue, self.get_rows("venues", {"ref_name": ref_name})[0]
         )
 
-    def venue_in_database(self, venue: models.Venue) -> bool:
-        """Returns True if `venue` is already in the database.
-        Database connection will be closed after calling this function."""
-        venue_dict = venue.flattened_dict
-        venue_dict.pop("date_added")
-        return self.count("venues", venue_dict) > 0
+    def get_venues(self, *args, **kwargs) -> list[models.Venue]:
+        """Return a list of `Venue` models.
+
+        `*args` and `**kwargs` can be any parameters accepted by `self.get_rows()`, except `table`:
+
+        * `match_criteria: list[tuple] | dict | None = None`
+        * `exact_match: bool = True`
+        * `sort_by_column: str | None = None`
+        * `columns_to_return: list[str] | None = None`
+        * `return_as_dataframe: bool = False`
+        * `values_only: bool = False`
+        * `order_by: str | None = None`
+        * `limit: str | int | None = None`
+        """
+        rows = self.get_rows("venues", *args, **kwargs)
+        return [dacite.from_dict(models.Venue, row) for row in rows]
 
     def update_in_the_future(self):
         """Set `in_the_future` column in the events table to `0` for events that have already happened."""
@@ -174,17 +178,12 @@ class GigBased(DataBased):
             f"UPDATE events SET in_the_future = 0 WHERE date < '{datetime.now()}';"
         )
 
-    def drop_future_events(self, venue: models.Venue | None = None):
-        """Delete events that haven't happened yet for `venue`.
-        If `venue` is not given, the operation will occur on all venues.
-
-        #### :Intention: Avoid adding duplicate events or needing to determine
-        if the event to be added already exists but website information for it has changed.
-        Ideally a backup of the database should be made first and this should be called after calling `self.update_in_the_future`."""
-        if venue:
-            self.delete("events", {"venue": venue.name, "in_the_future": 1})
-        else:
-            self.delete("events", {"in_the_future": 1})
+    def venue_in_database(self, venue: models.Venue) -> bool:
+        """Returns True if `venue` is already in the database.
+        Database connection will be closed after calling this function."""
+        venue_dict = venue.flattened_dict
+        venue_dict.pop("date_added")
+        return self.count("venues", venue_dict) > 0
 
 
 if __name__ == "__main__":
