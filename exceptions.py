a62454593831d4cd94e2d4695d8268a742345f6d
matt manes <mattmanes@pm.me>
2023-06-19 19:49:56 -0500

    style: change single quotes to double quotes

--- exceptions.py
+++ exceptions.py
@@ -10,4 +10,4 @@ class GigError(RuntimeError):
 
 class VenueExistsError(GigError):
     def __init__(self, venue: str):
-        super().__init__(f"'{venue}' already exists in the database.")
+        super().__init__(f'"{venue}" already exists in the database.')
