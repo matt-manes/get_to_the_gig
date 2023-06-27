69a960c5c7a40fcbb3a06e5f1e7f26c163a0ecd0
matt manes <mattmanes@pm.me>
2023-06-26 20:14:34 -0500

    refactor: update to new infrastructure

--- scrapers/liars_club.py
+++ scrapers/liars_club.py
@@ -1,42 +1,63 @@
-import time
 from datetime import datetime
+
+from bs4 import BeautifulSoup
+from gig_scraper import GigScraper
 from pathier import Pathier
-import json
 
-from gig_scraper_engine import GigScraper, get_text, get_page
+root = Pathier(__file__).parent
+(root.parent).add_to_PATH()
+
+import models
 
 
-class Scraper(GigScraper):
-    def __init__(self):
-        super().__init__(Pathier(__file__))
+# not_ready (This file will be ignored by scrape_venues.py until this comment is removed.)
+# calendar url: https://cdn5.editmysite.com/app/store/api/v28/editor/users/131312941/sites/715381155365579768/products?per_page=60&categories[]=11eb8686dd0a0704b0c60cc47a2b63cc
+class Venue(GigScraper):
+    @property
+    def name(self) -> str:
+        return Pathier(__file__).stem
 
-    def scrape(self):
-        self.logger.info("Scrape started")
+    def get_events(self) -> list[dict | BeautifulSoup]:
+        response = self.get_calendar()
+        return response.json()["data"]
+
+    def parse_event(self, data: dict | BeautifulSoup) -> models.Event | None:
         try:
-            data = get_page(
-                "https://cdn5.editmysite.com/app/store/api/v28/editor/users/131312941/sites/715381155365579768/products?per_page=60&categories[]=11eb8686dd0a0704b0c60cc47a2b63cc"
-            ).json()["data"]
-            for event in data:
-                self.reset_event_details()
-                details = event["product_type_details"]
-                self.event_date = datetime.strptime(
-                    f"{details['start_date']} {details['start_time']}",
-                    "%Y-%m-%d %H:%M %p",
-                )
-                self.title = event["name"]
-                high = event["price"]["high_formatted"]
-                low = event["price"]["low_formatted"]
-                if high == low:
-                    self.price = high
-                else:
-                    self.price = f"{low}-{high}"
-                self.event_link = f"{self.venue_info['website']}{event['site_link']}"
-                self.add_event()
+            event = models.Event.new()
+            details = data["product_type_details"]
+            event.date = datetime.strptime(
+                f"{details['start_date']} {details['start_time']}", "%Y-%m-%d %H:%M %p"
+            )
+            event.title = data["name"]
+            high = data["price"]["high_formatted"]
+            low = data["price"]["low_formatted"]
+            if high == low:
+                event.price = high
+            else:
+                event.price = f"{low}-{high}"
+            event.url = f"{self.venue.url}/{data['site_link']}"
+            return event
+        except Exception:
+            self.event_fail(event)
+            return None
 
-            self.log_success()
-        except:
-            self.logger.exception(f"Scrape failed")
+    @GigScraper.chores
+    def scrape(self):
+        try:
+            try:
+                events = self.get_events()
+            except Exception:
+                self.logger.exception("Error in get_events().")
+            else:
+                for listing in events:
+                    event = self.parse_event(listing)
+                    if event:
+                        self.add_event(event)
+        except Exception as e:
+            self.logger.exception("Unexpected failure.")
 
 
 if __name__ == "__main__":
-    Scraper().scrape()
+    venue = Venue()
+    venue.scrape()
+    print(venue.last_log)
