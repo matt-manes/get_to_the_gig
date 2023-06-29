13439515716f209a8673de1da7686f1e04f09f9d
matt manes <mattmanes@pm.me>
2023-06-28 20:22:03 -0500

    refactor: update to new infrastructure

--- scrapers/the_burlington.py
+++ scrapers/the_burlington.py
@@ -1,38 +1,47 @@
-from datetime import datetime, timedelta
-from pathier import Pathier
 import json
-from typing import Iterable
+from datetime import datetime
 
-from gig_scraper_engine import GigScraper, get_soup, get_text, get_page
+from bs4 import BeautifulSoup
+from gig_scraper import GigScraper
+from pathier import Pathier
 
-# https://www.theburlingtonbar.com/
-class Scraper(GigScraper):
-    def __init__(self):
-        super().__init__(Pathier(__file__))
+root = Pathier(__file__).parent
+(root.parent).add_to_PATH()
 
-    def scrape(self):
-        self.logger.info("Scrape started")
+import models
+
+
+# calendar url: https://www.theburlingtonbar.com
+class Venue(GigScraper):
+    @property
+    def name(self) -> str:
+        return Pathier(__file__).stem
+
+    def get_events(self) -> list[dict]:
+        response = self.get_calendar()
+        soup = self.as_soup(response)
+        collection_id = json.loads(
+            soup.find("div", class_="sqs-block calendar-block sqs-block-calendar").get(
+                "data-block-json"
+            )
+        )["collectionId"]
+        return self.get_squarespace_events(collection_id)
+
+    def parse_event(self, data: dict) -> models.Event | None:
         try:
-            soup = get_soup(self.venue_info["calendar_url"])
-            collection_id = json.loads(
-                soup.find(
-                    "div", class_="sqs-block calendar-block sqs-block-calendar"
-                ).get("data-block-json")
-            )["collectionId"]
-            for month in self.get_squarespace_calendar(collection_id):
-                for event in month:
-                    self.reset_event_details()
-                    self.event_date = datetime.fromtimestamp(event["startDate"] / 1000)
-                    self.title = event["title"]
-                    self.acts = self.title
-                    self.price = "$10"
-                    self.event_link = f"{self.venue_info['calendar_url'].strip('/')}{event['fullUrl']}"
-                    self.add_event()
-            self.log_success()
-        except Exception as e:
-            print(e)
-            self.logger.exception(e)
+            event = models.Event.new()
+            event.date = datetime.fromtimestamp(int(data["startDate"] / 1000))
+            event.title = data["title"]
+            event.acts = event.title
+            event.price = "$10"
+            event.url = f"{self.venue.calendar_url}/{data['fullUrl']}"
+            return event
+        except Exception:
+            self.event_fail(event)
+            return None
 
 
 if __name__ == "__main__":
-    Scraper().scrape()
+    venue = Venue()
+    venue.scrape()
+    print(venue.last_log)
