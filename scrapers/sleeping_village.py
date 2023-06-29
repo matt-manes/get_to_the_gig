f0bd0cc23cfcfb85451471aa29e3ae32262d2508
matt manes <mattmanes@pm.me>
2023-06-28 20:16:49 -0500

    refactor: update to new infrastructure

--- scrapers/sleeping_village.py
+++ scrapers/sleeping_village.py
@@ -1,47 +1,59 @@
-import datetime
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+from gig_scraper import GigScraper
 from pathier import Pathier
 
-from gig_scraper_engine import GigScraper, get_soup, get_text, get_page
+root = Pathier(__file__).parent
+(root.parent).add_to_PATH()
 
-# https://sleeping-village.com/events/
-class Scraper(GigScraper):
-    def __init__(self):
-        super().__init__(Pathier(__file__))
+import models
 
-    def scrape(self):
-        self.logger.info("Scrape started")
+
+# calendar url: https://sleeping-village.com/events
+class Venue(GigScraper):
+    @property
+    def name(self) -> str:
+        return Pathier(__file__).stem
+
+    def get_events(self) -> list[dict]:
+        events = []
+        for i in range(1, 6):
+            events.extend(
+                self.get_page(
+                    f"https://sleeping-village.com/api/plot/v1/listings?page={i}"
+                ).json()
+            )
+        # input(events)
+        return events
+
+    def parse_event(self, data: dict) -> models.Event | None:
         try:
-            events = []
-            for i in range(1, 6):
-                events.extend(
-                    get_page(
-                        f"https://sleeping-village.com/api/plot/v1/events?page={i}"
-                    ).json()
+            event = models.Event.new()
+            event.title = data["title"]
+            event.date = datetime.strptime(
+                data["dateTime"], "<span>%a, %b %d %H:%M%p<span>"
+            )
+            event.date = event.date.replace(year=2023)
+            event = self.check_event_year(event)
+            if "lineup" in data:
+                event.acts = ", ".join(
+                    act["title"] for act in data["lineup"]["standard"]
                 )
-            for event in events:
-                self.reset_event_details()
-                self.event_date = datetime.datetime.strptime(
-                    event["dateTime"], "<span>%a, %b %d %H:%M%p<span>"
-                )
-                self.event_date = self.event_date.replace(year=2023)
-                self.check_event_date_year()
-                self.title = event["title"]
-                if "lineup" in event:
-                    self.acts = ", ".join(
-                        act["title"] for act in event["lineup"]["standard"]
-                    )
-                else:
-                    self.acts = self.title
-                self.price = f'${event["tickets"][0]["price"]}'
-                if self.price == "$0":
-                    self.price = "Free"
-                self.event_link = event["permalink"]
-                self.info = event["description"]
-                self.add_event()
-            self.log_success()
-        except:
-            self.logger.exception(f"Scrape failed")
+            else:
+                event.acts = event.title
+            event.price = f"${data['tickets'][0]['price']}"
+            if event.price == "$0":
+                event.price = "Free"
+            event.url = data["permalink"]
+            event.info = data["description"]
+            return event
+        except Exception:
+            self.event_fail(event)
+            return None
 
 
 if __name__ == "__main__":
-    Scraper().scrape()
+    venue = Venue()
+    venue.scrape()
+    print(venue.last_log)
