7f2783debb7dcc67d7787fc2c7bfab436c99e11e
matt manes <mattmanes@pm.me>
2023-06-29 20:30:04 -0500

    feat: add fields to config

--- config.py
+++ config.py
@@ -12,6 +12,8 @@ class Config:
     drop_future_events: bool
     update_in_the_future: bool
     drop_all_events: bool
+    default_event_display_columns: list[str]
+    default_event_column_order: list[str]
 
     @classmethod
     def load(cls, path: Pathish = Pathier(__file__).parent / "config.toml") -> Self:
@@ -23,17 +25,3 @@ class Config:
         """Write the contents of this `datamodel` object to `path`."""
         data = asdict(self)
         Pathier(path).dumps(data)
-
-    def all_chores_on(self):
-        """Set all chores to `True`."""
-        self.backup_before_scrape = True
-        self.drop_future_events = True
-        self.update_in_the_future = True
-        self.drop_all_events = True
-
-    def all_chores_off(self):
-        """Set all chores to `False`."""
-        self.backup_before_scrape = False
-        self.drop_future_events = False
-        self.update_in_the_future = False
-        self.drop_all_events = False
