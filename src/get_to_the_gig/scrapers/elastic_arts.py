6a010fdf3a85d94f70b6761d8106c459c26c8215
matt manes <mattmanes@pm.me>
2024-07-22 15:36:53 -0500

    refactor: update framework usage

--- src/get_to_the_gig/scrapers/elastic_arts.py
+++ src/get_to_the_gig/scrapers/elastic_arts.py
@@ -1,42 +1,54 @@
 import re
 from datetime import datetime
 
-from gig_scraper import GigScraper
-from pathier import Pathier
+import gruel
+from typing_extensions import Any, Type, override
 
-root = Pathier(__file__).parent
-(root.parent).add_to_PATH()
-
-import models
+from get_to_the_gig import event_parser
+from get_to_the_gig.giggruel import GigGruel
+from get_to_the_gig.squarespace import SquarespaceCalendar
 
 
 # calendar url: https://elasticarts.org/events
-class VenueScraper(GigScraper):
+class EventParser(event_parser.EventParser):
     @property
-    def name(self) -> str:
-        return Pathier(__file__).stem
+    @override
+    def item(self) -> dict[str, Any]:
+        return self._item
 
-    def get_events(self) -> list[dict]:
-        response = self.get_calendar()
+    # Add `_parse_` functions below
+    def _parse_all(self) -> None:
+        self.event.date = datetime.fromtimestamp(int(self.item["startDate"] / 1000))
+        self.event.title = self.item["title"]
+        self.event.price = "$15+"
+        self.event.url = f"{self.event.venue.url}/{self.item['fullUrl']}"
+
+
+class VenueScraper(GigGruel):
+    @property
+    @override
+    def event_parser(self) -> Type[EventParser]:
+        return EventParser
+
+    # Default requests `venue.calendar_url` and returns the response
+    # Uncomment and override if needed
+    # @override
+    # def get_source(self) -> gruel.Response:
+    #    raise NotImplementedError
+
+    @override
+    def get_parsable_items(self, source: gruel.Response) -> list[dict[str, Any]]:
         collection_id = re.findall(
-            r'collectionId":"[a-zA-Z0-9]+"', response.text.replace("&quot;", '"')
+            r'collectionId":"[a-zA-Z0-9]+"', source.text.replace("&quot;", '"')
         )[0].split('"')[2]
-        return self.get_squarespace_events(collection_id)
-
-    def parse_event(self, data: dict) -> models.Event | None:
-        try:
-            event = models.Event.new()
-            event.date = datetime.fromtimestamp(int(data["startDate"] / 1000))
-            event.title = data["title"]
-            event.price = "$15+"
-            event.url = f"{self.venue.url}/{data['fullUrl']}"
-            return event
-        except Exception:
-            self.event_fail(event)
-            return None
+        return SquarespaceCalendar(
+            gruel.models.Url(self.venue.url), collection_id, self.logger
+        ).get_events()
 
 
 if __name__ == "__main__":
     venue = VenueScraper()
+    venue.show_parse_items_prog_bar = True
     venue.scrape()
-    print(venue.last_log)
+    print(f"{venue.success_count=}")
+    print(f"{venue.fail_count=}")
