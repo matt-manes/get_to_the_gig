3f882d3db31e9491c408965f9fcc83d4d3266f31
matt manes <mattmanes@pm.me>
2024-07-28 17:07:31 -0500

    refactor: update scraper

--- src/get_to_the_gig/scrapers/the_burlington.py
+++ src/get_to_the_gig/scrapers/the_burlington.py
@@ -1,46 +1,64 @@
+import datetime
 import json
-from datetime import datetime
 
-from gig_scraper import GigScraper
-from pathier import Pathier
+import gruel
+from bs4 import Tag
+from typing_extensions import Any, Type, override
 
-root = Pathier(__file__).parent
-(root.parent).add_to_PATH()
-
-import models
-
+from get_to_the_gig import event_parser, exceptions
+from get_to_the_gig.giggruel import GigGruel
+from get_to_the_gig.squarespace import SquarespaceCalendar
 
 # calendar url: https://www.theburlingtonbar.com
-class VenueScraper(GigScraper):
+
+
+class EventParser(event_parser.EventParser):
     @property
-    def name(self) -> str:
-        return Pathier(__file__).stem
+    @override
+    def item(self) -> dict[str, Any]:  # change `Any` to appropriate type
+        return self._item
 
-    def get_events(self) -> list[dict]:
-        response = self.get_calendar()
-        soup = self.as_soup(response)
-        collection_id = json.loads(
-            soup.find("div", class_="sqs-block calendar-block sqs-block-calendar").get(
-                "data-block-json"
+    # Add `_parse_` functions below
+    def _parse_all(self) -> None:
+        self.event.title = self.item["title"]
+        self.event.acts = self.event.title
+        self.event.date = datetime.datetime.fromtimestamp(
+            int(self.item["startDate"] / 1000)
+        )
+        self.event.url = f"{self.event.venue.calendar_url}/{self.item['fullUrl']}"
+        self.event.price = "$10"
+
+
+class VenueScraper(GigGruel):
+    @property
+    @override
+    def event_parser(self) -> Type[EventParser]:
+        return EventParser
+
+    # Default requests `venue.calendar_url` and returns the response
+    # Uncomment and override if needed
+    # @override
+    # def get_source(self) -> gruel.Response:
+    #    raise NotImplementedError
+
+    @override
+    def get_parsable_items(self, source: gruel.Response) -> list[dict[str, Any]]:
+        soup = source.get_soup()
+        block = soup.find("div", class_="sqs-block calendar-block sqs-block-calendar")
+        if not isinstance(block, Tag):
+            raise exceptions.MissingElementError(
+                "<div class='sqs-block calendar-block sqs-block-calendar'>"
             )
-        )["collectionId"]
-        return self.get_squarespace_events(collection_id)
-
-    def parse_event(self, data: dict) -> models.Event | None:
-        try:
-            event = models.Event.new()
-            event.date = datetime.fromtimestamp(int(data["startDate"] / 1000))
-            event.title = data["title"]
-            event.acts = event.title
-            event.price = "$10"
-            event.url = f"{self.venue.calendar_url}/{data['fullUrl']}"
-            return event
-        except Exception:
-            self.event_fail(event)
-            return None
+        collection_id = json.loads(str(block.get("data-block-json")))["collectionId"]
+        return SquarespaceCalendar(
+            gruel.models.Url(self.venue.url), collection_id, self.logger
+        ).get_events()
 
 
 if __name__ == "__main__":
     venue = VenueScraper()
+    venue.show_parse_items_prog_bar = True
+    # venue.test_mode = True
     venue.scrape()
-    print(venue.last_log)
+    print(f"{venue.success_count=}")
+    print(f"{venue.fail_count=}")
