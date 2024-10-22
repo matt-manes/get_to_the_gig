b776b5bfdec7abda2c8f25065541e81b4eded58e
matt manes <mattmanes@pm.me>
2024-10-22 13:08:59 -0500

    feat: add future, past, and today event views

--- src/get_to_the_gig/sql/views.sql
+++ src/get_to_the_gig/sql/views.sql
@@ -1,19 +1,77 @@
 CREATE VIEW IF NOT EXISTS
     events_view AS
 SELECT
-    event_id,
-    events.venue_id,
+    event_id AS eid,
+    events.venue_id AS vid,
     title,
     date,
-    venues.name as venue,
+    venues.name AS venue,
     acts,
     price,
     ticket_url,
-    events.date_added,
-    CASE
-        WHEN CAST(JULIANDAY (date) - JULIANDAY ('now') AS INT) >= 0 THEN 1
-        ELSE 0
-    END in_the_future
+    events.date_added
 FROM
     events
     INNER JOIN venues ON events.venue_id = venues.venue_id;
+
+CREATE VIEW IF NOT EXISTS
+    future_events AS
+SELECT
+    event_id AS eid,
+    events.venue_id AS vid,
+    title,
+    date,
+    venues.name AS venue,
+    acts,
+    price,
+    ticket_url,
+    events.date_added
+FROM
+    events
+    INNER JOIN venues ON events.venue_id = venues.venue_id
+WHERE
+    CAST(JULIANDAY (date) - JULIANDAY ('now') AS INT) >= 0
+ORDER BY
+    date;
+
+CREATE VIEW IF NOT EXISTS
+    past_events AS
+SELECT
+    event_id AS eid,
+    events.venue_id AS vid,
+    title,
+    date,
+    venues.name AS venue,
+    acts,
+    price,
+    ticket_url,
+    events.date_added
+FROM
+    events
+    INNER JOIN venues ON events.venue_id = venues.venue_id
+WHERE
+    CAST(JULIANDAY (date) - JULIANDAY ('now') AS INT) < 0
+ORDER BY
+    date;
+
+CREATE VIEW IF NOT EXISTS
+    today AS
+SELECT
+    event_id AS eid,
+    events.venue_id AS vid,
+    title,
+    date,
+    venues.name AS venue,
+    acts,
+    price,
+    ticket_url,
+    events.date_added
+FROM
+    events
+    INNER JOIN venues ON events.venue_id = venues.venue_id
+WHERE
+    CAST(JULIANDAY (date) - JULIANDAY ('now') AS INT) = 0
+    AND JULIANDAY (date) > JULIANDAY ('now')
+    AND JULIANDAY (date) < JULIANDAY (date('now', '+1 day'))
+ORDER BY
+    date;
